75c7011fd32a0be3fb93d2cca802ffc44cd25d75
gulshan31 <k2caher@gmail.com>
2020-03-18 19:15:42 +0530

    #Admin Setup

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+    // admin section
+Route::get('/admin',function(){
+    return view('admin.welcome');
+});    
+// end of admin section
